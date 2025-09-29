0ad3270fc1f213bbd3a34e628529b25ab6839716
MattCantor <matthewcantor@gmail.com>
2025-09-29 15:16:26 -0400

    clean up

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,7 +1,7 @@
 import { Command } from "commander";
 import { parse } from "@vestlang/dsl";
-import { toCNF } from "@vestlang/normalizer";
-import { lint } from "@vestlang/linter";
+// import { toCNF } from "@vestlang/normalizer";
+// import { lint } from "@vestlang/linter";
 
 const program = new Command();
 
@@ -16,23 +16,23 @@ program
     console.log(JSON.stringify(ast, null, 2));
   });
 
-program
-  .command("cnf")
-  .argument("<input...>")
-  .action((parts: string[]) => {
-    const input = parts.join(" ");
-    const ast = parse(input);
-    console.log(JSON.stringify(toCNF(ast), null, 2));
-  });
+// program
+//   .command("cnf")
+//   .argument("<input...>")
+//   .action((parts: string[]) => {
+//     const input = parts.join(" ");
+//     const ast = parse(input);
+//     console.log(JSON.stringify(toCNF(ast), null, 2));
+//   });
 
-program
-  .command("lint")
-  .argument("<input...>")
-  .action((parts: string[]) => {
-    const input = parts.join(" ");
-    const ast = parse(input);
-    const issues = lint(ast);
-    console.log(issues);
-  });
+// program
+//   .command("lint")
+//   .argument("<input...>")
+//   .action((parts: string[]) => {
+//     const input = parts.join(" ");
+//     const ast = parse(input);
+//     const issues = lint(ast);
+//     console.log(issues);
+//   });
 
 program.parseAsync();
